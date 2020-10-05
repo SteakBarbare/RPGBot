099996f08268b893fb91529d662b9aed98ffd6bd
Corto Dufour <cortodufour@gmail.com>
2020-10-05 11:49:58 +0200

    Feature: Bot connection OK

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/SteakBarbare/RPGBot/handlers"
+	"github.com/bwmarrin/discordgo"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+const token string = "NzYxMjU4NDczNTYzNDg4MjY3.X3X_Mw.j4KGRVbhP0WZVoHBYlmNTHqvPzM"
+
+func main() {
+
+	// Create a new Discord session using the provided bot token.
+
+	dg, err := discordgo.New(`Bot ` + token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	// Register the messageCreate func as a callback for MessageCreate events.
+	dg.AddHandler(handlers.MessageCreate)
+
+	// In this example, we only care about receiving message events.
+	dg.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsGuildMessages)
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
